fef992f411e5cf520fb08db1e0dce8bd27cca596
4ur1Xlol <121487977+4ur1Xlol@users.noreply.github.com>
2022-12-27 16:02:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    speed: 0.5,
+    autoplay: true,
+    path: 'https://lottie.host/30559aee-dfd8-4782-a81e-662c5781b509/pWkhkB3Qkv.json'
+})
